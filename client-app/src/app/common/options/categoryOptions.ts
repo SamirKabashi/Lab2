50561ebbd14330638aed18821f6a926acb7df18c
Samir Kabashi <sk52317@ubt-uni.net>
2023-04-30 10:05:00 +0200

    categoryOptions(dropdown)

---
+++ client-app/src/app/common/options/categoryOptions.ts
@@ -0,0 +1,8 @@
+export const categoryOptions = [
+    {text: 'Memorial Complex Adem Jashari', value: 'Memorial Complex Adem Jashari'},
+    {text: 'Kalaja Prizrenit', value: 'Kalaja Prizrenit'},
+    {text: 'Cineplexx', value: 'cineplexx'},
+    {text: 'Burgerking', value: 'burgerking'},
+    {text: 'SunnnyHillFestival', value: 'SunnnyHillFestival'},
+    {text: 'TravelToDurres', value: 'travelToDurres'},
+]
